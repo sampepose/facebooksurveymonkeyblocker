fb094d902e4798312eba321efd6c4108f0ad0bb2
Sam Pepose <sam.pepose@oasisdigital.com>
2014-09-22 20:12:22 -0500

    not as specific filtering...

--- remove.js
+++ remove.js
@@ -1,7 +1,7 @@
 var INTERVAL_PERIOD = 200;
 
 window.setInterval(function() {
-    var surveyPosts = $('#contentArea ._4-u2:contains(surveymonkey)');
+    var surveyPosts = $('#contentArea ._4-u2:contains(surveymonkey.com)');
     if (surveyPosts.length) {
         surveyPosts.remove();
     }
